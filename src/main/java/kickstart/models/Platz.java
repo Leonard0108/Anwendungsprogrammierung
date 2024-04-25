1270ce3b4c1643b1223420c16c72af6a89b90cbe
Pando <pando@gmx.de>
2024-04-25 17:28:23 +0200

    Platz Modell-Klasse hinzugefügt

---
+++ src/main/java/kickstart/models/Platz.java
@@ -0,0 +1,52 @@
+package kickstart.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "PLACES")
+public class Platz {
+	private int raw;
+	private int number;
+
+	@Id
+	private Long id;
+
+	public Platz(int raw, int number) {
+		this.raw = raw;
+		this.number = number;
+		this.id = 100L * raw + number;
+	}
+
+	public Platz() {}
+
+	public int getRaw() {
+		return this.raw;
+	}
+
+	public int getNumber() {
+		return this.number;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	@Override
+	public int hashCode() {
+		return Math.toIntExact(this.id);
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if(this == object) return true;
+
+		if(object instanceof Platz place) {
+			return Objects.equals(place.getId(), this.getId());
+		}
+		return false;
+	}
+}

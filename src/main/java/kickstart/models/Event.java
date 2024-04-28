8b0afc3fb449e1e3ddf38589b58b28b9ba280007
Tobias <tobiasknoll2003@protonmail.com>
2024-04-28 14:35:11 +0200

    Datum und Uhrzeit zu LocalDateTime zusammengeführt

--- src/main/java/kickstart/models/Event.java
+++ src/main/java/kickstart/models/Event.java
@@ -6,21 +6,22 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
 @Entity
 @Table(name = "EVENTS")
 public class Event {
 
 	private @Id	@GeneratedValue long id;
 	private String name;
-	private String date;
-	private String time;
+	private LocalDateTime startDateTime;
 	// Dauer in Minuten
 	private int duration;
 
-	public Event(String name, String date, String time, int duration) {
+	public Event(String name, LocalDateTime startDateTime, int duration) {
 		this.name = name;
-		this.date = date;
-		this.time = time;
+		this.startDateTime = startDateTime;
 		this.duration = duration;
 	}
 
@@ -34,15 +35,31 @@ public class Event {
 		return name;
 	}
 
-	public String getDate() {
-		return date;
-	}
-
-	public String getTime() {
-		return time;
+	public LocalDateTime getStartDateTime() {
+		return startDateTime;
 	}
 
 	public int getDuration() {
 		return duration;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Math.toIntExact(this.id);
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if(this == object) return true;
+
+		if(object instanceof Event event) {
+			return Objects.equals(event.getId(), this.getId());
+		}
+		return false;
+	}
+
+	public int compareTo(Event event) {
+		return this.getStartDateTime().compareTo(event.getStartDateTime());
 	}
 }

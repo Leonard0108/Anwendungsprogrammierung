d9701a1c8c9762c61ecdd8d58c4dceb91f5551db
Pando <pando@gmx.de>
2024-05-01 12:21:18 +0200

    ViewProgramController helper Methoden zur Datums/Zeitverwaltung hinzugefügt und getCurrentProgram(...) angepasst

--- src/main/java/kickstart/controller/ViewProgramController.java
+++ src/main/java/kickstart/controller/ViewProgramController.java
@@ -10,6 +10,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.WeekFields;
+import java.util.Locale;
+
 /**
  *
  * @author Jannik
@@ -22,8 +27,56 @@ public class ViewProgramController {
      * @param week
      * @param m 
      */
-    @GetMapping("/current-films/{week}")
-    public void getCurrentProgram(@PathVariable String week , Model m){}
-    
-    
+    @GetMapping("/current-films/{year}/{week}")
+    public String getCurrentProgram(@PathVariable int year, @PathVariable int week , Model m) {
+
+		System.out.println("Start der Woche: " + getStartWeekDateTime(year, week));
+		System.out.println("Ende der Woche: " + getEndWeekDateTime(year, week));
+
+		return "welcome";
+	}
+
+	// Helper Methoden
+	// TODO: Später in einen Service verschieben
+
+	public static int getMaxYearWeeks(int year) {
+		return getWeekOfYear(LocalDateTime.of(year, 12, 31, 0, 0));
+	}
+
+	public static int getWeekOfYear(LocalDateTime dateTime) {
+		return dateTime
+			.get(WeekFields.of(Locale.getDefault()).weekOfWeekBasedYear());
+	}
+
+	// Chat GPT 3.5
+	// Promt: Wie bekomme ich das Start-Datum zu einer bestimmten Woche?
+	public static LocalDateTime getStartWeekDateTime(int year, int week) {
+		WeekFields weekFields = WeekFields.of(Locale.getDefault());
+
+		return LocalDateTime.of(year, 1, 1, 0, 0)
+			.with(weekFields.weekOfYear(), week)
+			.with(weekFields.dayOfWeek(), 1);
+	}
+
+	public static LocalDateTime getStartWeekDateTime(LocalDateTime dateTime) {
+		return getStartWeekDateTime(
+			dateTime.getYear(),
+			getWeekOfYear(dateTime)
+		);
+	}
+
+	public static LocalDateTime getEndWeekDateTime(int year, int week) {
+		WeekFields weekFields = WeekFields.of(Locale.getDefault());
+
+		return LocalDateTime.of(year, 1, 1, 23, 59)
+			.with(weekFields.weekOfYear(), week)
+			.with(weekFields.dayOfWeek(), 7);
+	}
+
+	public static LocalDateTime getEndWeekDateTime(LocalDateTime dateTime) {
+		return getEndWeekDateTime(
+			dateTime.getYear(),
+			getWeekOfYear(dateTime)
+		);
+	}
 }

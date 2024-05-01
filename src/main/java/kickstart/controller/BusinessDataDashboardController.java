d9ee0f88608c5bf17b4fd0b5f5447501e1253114
Tobias <tobiasknoll2003@protonmail.com>
2024-05-01 17:02:33 +0200

    erste Controller angelegt

---
+++ src/main/java/kickstart/controller/BusinessDataDashboardController.java
@@ -0,0 +1,16 @@
+package kickstart.controller;
+
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+//benötigte Klassen: Bestellung, (Ausgaben für Personalkosten, Ausgaben für momentan geliehene Filme)
+@Controller
+public class BusinessDataDashboardController {
+
+	@GetMapping("/business-data-dashboard")
+	@PreAuthorize("hasRole('BOSS')")
+	public void getDashboard(Model m){}
+}
---
+++ src/main/java/kickstart/controller/EventAdministrationController.java
@@ -0,0 +1,4 @@
+package kickstart.controller;
+
+public class EventAdministrationController {
+}

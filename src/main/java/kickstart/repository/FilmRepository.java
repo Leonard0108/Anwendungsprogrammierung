b492dfc8f7798ce9651a5bd66da4db3ca535c72d
Pando <pando@gmx.de>
2024-05-01 10:51:40 +0200

    FilmRepository Klasse erweitert

--- src/main/java/kickstart/repository/FilmRepository.java
+++ src/main/java/kickstart/repository/FilmRepository.java
@@ -4,13 +4,17 @@
  */
 package kickstart.repository;
 
+import kickstart.models.CinemaShow;
 import kickstart.models.Film;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.util.Streamable;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author Jannik
  */
+@Repository
 public interface FilmRepository extends CrudRepository<Film, Long>{
-    
+	Streamable<Film> findAll();
 }

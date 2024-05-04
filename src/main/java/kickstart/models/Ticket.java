8978d23876a1148b105571f44d071f586c180248
SimonBanks42 <sl-peace@gmx.de>
2024-05-04 15:43:21 +0200

    Klassen Snacks, Ticket und Bestellung hinzugefügt und Snacks Initializer geschrieben

--- src/main/java/kickstart/models/Ticket.java
+++ src/main/java/kickstart/models/Ticket.java
@@ -2,55 +2,58 @@ package kickstart.models;
 
 import java.util.Objects;
 
+import org.javamoney.moneta.Money;
+
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotEmpty;
 
 public class Ticket implements Comparable<Ticket> {
 
-    public static enum TicketKategorie {
+    public static enum TicketCategory {
         normal,
         reduced,
         children
     }
 
-    /*
-     * Ticket(TicketKategorie Kategorie, CinemaShow show) {
-     * this.kategorie = Kategorie;
-     * double reduction;
-     * switch (Kategorie) {
-     * case reduced:
-     * reduction = 0.8;
-     * case children:
-     * reduction = 0.7;
-     * default:
-     * reduction = 1;
-     * }
-     * ;
-     * this.TicketPreis = show.getBasePrice() * reduction;
-     * }
-     */
-    Ticket() {
-
-    }
-
     @NotEmpty
     private @Id @GeneratedValue Long id;
 
-    private TicketKategorie kategorie;
+    private TicketCategory category;
+    private Money TicketPrice;
+    // private CinemaShow show;
+    // private Reservation reservation;
 
-    private double TicketPreis;
+    Ticket(TicketCategory Category/* , CinemaShow cinemaShow */) {
+        this.category = Category;
+        double reduction;
+        switch (this.category) {
+            case reduced:
+                reduction = 0.8;
+            case children:
+                reduction = 0.7;
+            default:
+                reduction = 1;
+        }
+        // this.show = cinemaShow;
+        // this.TicketPreis = show.getBasePrice() * reduction;
+
+    }
+
+    Ticket() {
+
+    }
 
     public Long getId() {
         return this.id;
     }
 
-    public TicketKategorie getKategorie() {
-        return this.kategorie;
+    public TicketCategory getCategory() {
+        return this.category;
     }
 
-    public double getTicketPreis() {
-        return this.TicketPreis;
+    public Money getTicketPrice() {
+        return this.TicketPrice;
     }
 
     @Override
@@ -62,12 +65,13 @@ public class Ticket implements Comparable<Ticket> {
             return false;
 
         return Objects.equals(getId(), ticket.getId())
-                && Objects.equals(getKategorie(), ticket.getKategorie());
+                && Objects.equals(getCategory(), ticket.getCategory())
+                && Objects.equals(getTicketPrice(), ticket.getTicketPrice());
     }
 
     @Override
     public int compareTo(Ticket ticket) {
-        return (this.getId().equals(ticket.getId())) ? 0 : 1;
+        return (this.equals(ticket)) ? 0 : 1;
     }
 
 }

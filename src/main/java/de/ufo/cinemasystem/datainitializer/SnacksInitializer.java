5d71ecec9d3eecfe4f2e52b8084cf0df3ca09db3
Pando <pando@gmx.de>
2024-05-09 00:51:21 +0200

    Snack-Klassen angepasst

--- src/main/java/de/ufo/cinemasystem/datainitializer/SnacksInitializer.java
+++ src/main/java/de/ufo/cinemasystem/datainitializer/SnacksInitializer.java
@@ -9,7 +9,11 @@ import de.ufo.cinemasystem.repository.SnacksRepository;
 import java.util.Random;
 
 import org.javamoney.moneta.Money;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
 
+@Component
+@Order(10)
 public class SnacksInitializer implements DataInitializer {
 
     private final SnacksRepository snacksrepository;

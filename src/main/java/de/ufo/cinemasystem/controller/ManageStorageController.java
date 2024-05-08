ca2df25513eb5b448735808a0e60af72bc980220
Pando <pando@gmx.de>
2024-05-08 23:06:12 +0200

    Snacks Anzeigen HTML-Template mit Controller verknüpft

--- src/main/java/de/ufo/cinemasystem/controller/ManageStorageController.java
+++ src/main/java/de/ufo/cinemasystem/controller/ManageStorageController.java
@@ -1,11 +1,24 @@
 package de.ufo.cinemasystem.controller;
 
+import de.ufo.cinemasystem.repository.SnacksRepository;
 import org.salespointframework.catalog.Product;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ManageStorageController {
 
-	ManageStorageController() {
+	private SnacksRepository snacksRepository;
+
+	ManageStorageController(SnacksRepository snacksRepository) {
+		this.snacksRepository = snacksRepository;
+	}
+
+	@GetMapping("/manage-storage/")
+	public String showStorage(Model model) {
+		model.addAttribute("allSnacks", snacksRepository.findAll());
+
+		return "manage-storage";
 	}
 }

a0ad7ec6e6791d30a897eb034e7ff69f4c5273f6
Pando <pando@gmx.de>
2024-05-08 22:13:47 +0200

    Filme Ausleihen Anbieter anzeigen, HTML Template mit Controller verknüpft

--- src/main/java/de/ufo/cinemasystem/controller/RentFilmController.java
+++ src/main/java/de/ufo/cinemasystem/controller/RentFilmController.java
@@ -1,13 +1,23 @@
 package de.ufo.cinemasystem.controller;
 
+import de.ufo.cinemasystem.repository.FilmRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RentFilmController {
 
+	private FilmRepository filmRepository;
+
+	public RentFilmController(FilmRepository filmRepository) {
+		this.filmRepository = filmRepository;
+	}
+
 	@GetMapping("/rent-films/")
-	public String getRentFilms() {
-		return "films-rental-renderer";
+	public String getRentFilms(Model model) {
+		model.addAttribute("allFilms", filmRepository.findAll());
+
+		return "films-rental";
 	}
 }

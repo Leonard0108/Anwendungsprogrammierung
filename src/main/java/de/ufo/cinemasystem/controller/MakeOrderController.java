670eb17e783a7765b5dcd185eb52179384fb557f
git <sl-peace@gmx.de>
2024-05-17 08:53:58 +0200

    OrderController zugefügt

---
+++ src/main/java/de/ufo/cinemasystem/controller/MakeOrderController.java
@@ -0,0 +1,26 @@
+package de.ufo.cinemasystem.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import de.ufo.cinemasystem.repository.CinemaShowRepository;
+import de.ufo.cinemasystem.repository.OrderRepository;
+import de.ufo.cinemasystem.repository.ReservationRepository;
+import de.ufo.cinemasystem.repository.SnacksRepository;
+import de.ufo.cinemasystem.repository.UserRepository;
+
+@Controller
+public class MakeOrderController {
+
+	private @Autowired OrderRepository orderRepo;
+	private @Autowired ReservationRepository reservationRepo;
+	private @Autowired SnacksRepository snacksRepository;
+	private @Autowired CinemaShowRepository showsRepo;
+	private @Autowired UserRepository userRepo;
+
+	@GetMapping("/sell/")
+	public String startOrder() {
+		return "films-rental-renderer";
+	}
+}

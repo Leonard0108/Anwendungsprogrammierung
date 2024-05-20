d53e043df3340dfc0f573b1bb25fcdb015882dec
Pando <pando@gmx.de>
2024-05-20 15:13:08 +0200

    Sitzplätze besser ins System integriert

--- src/main/java/de/ufo/cinemasystem/datainitializer/CinemaHallDataInitializer.java
+++ src/main/java/de/ufo/cinemasystem/datainitializer/CinemaHallDataInitializer.java
@@ -1,25 +1,28 @@
 package de.ufo.cinemasystem.datainitializer;
 
+import de.ufo.cinemasystem.models.Seat;
+import de.ufo.cinemasystem.models.CinemaHallService;
 import org.salespointframework.core.DataInitializer;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
-import de.ufo.cinemasystem.models.CinemaHall;
 import de.ufo.cinemasystem.repository.CinemaHallRepository;
-import de.ufo.cinemasystem.repository.CinemaShowRepository;
 
-import java.util.HashMap;
+import java.util.AbstractMap;
 
 @Component
 @Order(3)
 public class CinemaHallDataInitializer implements DataInitializer {
 
 	private final CinemaHallRepository cinemaHallRepository;
+	private final CinemaHallService cinemaHallService;
 
-	public CinemaHallDataInitializer(CinemaHallRepository cinemaHallRepository) {
+	public CinemaHallDataInitializer(CinemaHallService cinemaHallService, CinemaHallRepository cinemaHallRepository) {
+		Assert.notNull(cinemaHallService, "cinemaHallService must not be null!");
 		Assert.notNull(cinemaHallRepository, "cinemaHallRepository must not be null!");
 
+		this.cinemaHallService = cinemaHallService;
 		this.cinemaHallRepository = cinemaHallRepository;
 	}
 
@@ -33,7 +36,20 @@ public class CinemaHallDataInitializer implements DataInitializer {
 
 		// Erstelle 6 Kinosäle:
 		for(int i = 0; i < 6; i++) {
-			cinemaHallRepository.save(new CinemaHall("Saal " + i));
+			cinemaHallService.createCinemaHall("Saal " + i,
+				new AbstractMap.SimpleEntry<>(18, Seat.PlaceGroup.GROUP_1),
+				new AbstractMap.SimpleEntry<>(18, Seat.PlaceGroup.GROUP_1),
+				new AbstractMap.SimpleEntry<>(18, Seat.PlaceGroup.GROUP_1),
+				new AbstractMap.SimpleEntry<>(18, Seat.PlaceGroup.GROUP_1),
+				new AbstractMap.SimpleEntry<>(18, Seat.PlaceGroup.GROUP_2),
+				new AbstractMap.SimpleEntry<>(18, Seat.PlaceGroup.GROUP_2),
+				new AbstractMap.SimpleEntry<>(18, Seat.PlaceGroup.GROUP_2),
+				new AbstractMap.SimpleEntry<>(18, Seat.PlaceGroup.GROUP_2),
+				new AbstractMap.SimpleEntry<>(18, Seat.PlaceGroup.GROUP_3),
+				new AbstractMap.SimpleEntry<>(18, Seat.PlaceGroup.GROUP_3),
+				new AbstractMap.SimpleEntry<>(18, Seat.PlaceGroup.GROUP_3),
+				new AbstractMap.SimpleEntry<>(18, Seat.PlaceGroup.GROUP_3)
+				);
 		}
 
 		System.out.println("Kinosäle:");

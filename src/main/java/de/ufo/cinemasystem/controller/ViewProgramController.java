76c791396905cb941e3db9f8e5aa717665d97a6d
Pando <pando@gmx.de>
2024-05-31 19:18:19 +0200

    UI-Kleinigkeiten ergänzt + Backend Eingabeprüfungen

--- src/main/java/de/ufo/cinemasystem/controller/ViewProgramController.java
+++ src/main/java/de/ufo/cinemasystem/controller/ViewProgramController.java
@@ -6,6 +6,7 @@ package de.ufo.cinemasystem.controller;
 
 import de.ufo.cinemasystem.additionalfiles.AdditionalDateTimeWorker;
 import de.ufo.cinemasystem.models.CinemaShowService;
+import de.ufo.cinemasystem.services.ScheduledActivityService;
 import org.javamoney.moneta.Money;
 import org.springframework.data.util.Streamable;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -22,6 +23,7 @@ import de.ufo.cinemasystem.models.Film;
 import de.ufo.cinemasystem.repository.CinemaHallRepository;
 import de.ufo.cinemasystem.repository.CinemaShowRepository;
 import de.ufo.cinemasystem.repository.FilmRepository;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -48,12 +50,16 @@ public class ViewProgramController {
 
 	private FilmRepository filmRepository;
 
+	private ScheduledActivityService scheduledActivityService;
+
 	public ViewProgramController(CinemaShowRepository cinemaShowRepository, CinemaShowService cinemaShowService,
-								 CinemaHallRepository cinemaHallRepository, FilmRepository filmRepository) {
+								 CinemaHallRepository cinemaHallRepository, FilmRepository filmRepository,
+								 ScheduledActivityService scheduledActivityService) {
 		this.cinemaShowRepository = cinemaShowRepository;
 		this.cinemaShowService = cinemaShowService;
 		this.cinemaHallRepository = cinemaHallRepository;
 		this.filmRepository = filmRepository;
+		this.scheduledActivityService = scheduledActivityService;
 	}
         
         @GetMapping("/current-films")
@@ -110,31 +116,57 @@ public class ViewProgramController {
 	}
 
 	@PostMapping("/current-films/{year}/{week}")
-	public String postNewProgram(@PathVariable int year, @PathVariable int week,
+	public String postNewProgram(RedirectAttributes redirectAttributes,
+								 @PathVariable int year, @PathVariable int week,
 								 @RequestParam("film") Long film, @RequestParam("room") Long room,
 								 //ChatGPT 3.5
 								 // Promt: Wie kann ich aus einem input Feld vom type="datetime-local ein LocalDateTime Objekt machen?
-								 @RequestParam("addTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime addTime,
-								 Model m) {
+								 @RequestParam("addTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime addTime) {
 
 		Optional<Film> optFilmInst = filmRepository.findById(film);
 		Optional<CinemaHall> optRoomInst = cinemaHallRepository.findById(room);
 		if(optFilmInst.isEmpty()) {
-			// TODO Fehlerbehandlung
+			// Dieser Fehler sollte im Regelfall nicht auftreten!
+			redirectAttributes.addFlashAttribute("errorMessage", "Der Film existiert nicht!");
 			return "redirect:/current-films/{year}/{week}";
 		}
 		if(optRoomInst.isEmpty()) {
-			// TODO Fehlerbehandlung
+			// Dieser Fehler sollte im Regelfall nicht auftreten!
+			redirectAttributes.addFlashAttribute("errorMessage", "Der Kinosaal existiert nicht!");
 			return "redirect:/current-films/{year}/{week}";
 		}
+
 		Film filmInst = optFilmInst.get();
-		if(!filmInst.isAvailableAt(addTime)) {
-			// TODO: Fehlerbehandlung
+		CinemaHall cinemaHallInst = optRoomInst.get();
+
+		if(addTime.isBefore(LocalDateTime.now().plusHours(24))) {
+			redirectAttributes.addFlashAttribute("errorMessage",
+				"Die Vorführung muss mind. 24 Stunden in der Zukunft liegen!");
 			return "redirect:/current-films/{year}/{week}";
 		}
-		// TODO: Prüfe ob sich Events oder Filme überlappen, wenn ja Abbruch und Fehler
+
+		if(!filmInst.isAvailableAt(addTime)) {
+			redirectAttributes.addFlashAttribute("errorMessage",
+				"Dieser Film wurde in der Zeit nicht ausgeliehen oder der Ticket-Preis wurde nicht gesetzt!");
+			return "redirect:/current-films/{year}/{week}";
+		}
+
+		// Prüfung, ob sich Events oder Vorführungen in dem Vorführungszeitraum und Kinosaal überlappen,
+		// wenn ja Abbruch und Fehler
+		if(!scheduledActivityService.isTimeSlotAvailable(
+			addTime, addTime.plusMinutes(filmInst.getTimePlaying()), cinemaHallInst.getId())) {
+			redirectAttributes.addFlashAttribute("errorMessage",
+				"In dem Kinosaal findet von Vorführungs-Beginn, bis Ende " +
+					" bereits eine oder mehrere andere Vorführungen oder Events statt!");
+			redirectAttributes.addFlashAttribute("infoMessage",
+				"Bis 20 min vor und nach Veranstaltungen/Events können keine Vorführungen gebucht werden!");
+			return "redirect:/current-films/{year}/{week}";
+		}
+
 		// Erstelle neue Vorführung
-		cinemaShowService.createCinemaShow(addTime, Money.of(9.99, EURO), optFilmInst.get(), optRoomInst.get());
+		redirectAttributes.addFlashAttribute("successMessage",
+			"Neue Vorführung wurde erfolgreich erstellt!");
+		cinemaShowService.createCinemaShow(addTime, Money.of(9.99, EURO), filmInst, cinemaHallInst);
 
 		return "redirect:/current-films/{year}/{week}";
 	}
@@ -155,16 +187,56 @@ public class ViewProgramController {
 	}
 
 	@PostMapping("/cinema-shows/{id}/edit")
-	public String editCinemaShow(@PathVariable Long id,
+	public String editCinemaShow(RedirectAttributes redirectAttributes,
+								 @PathVariable Long id,
 								 @RequestParam("film") Long film,
-								 @RequestParam("editTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime editTime,
-								 Model m) {
+								 @RequestParam("editTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime editTime) {
 		Optional<Film> optFilmInst = filmRepository.findById(film);
-		if(optFilmInst.isEmpty()) {
-			// TODO Fehlerbehandlung
-			return "redirect:/current-films/{year}/{week}";
+
+		Optional<CinemaShow> optCinemaShow = cinemaShowRepository.findById(id);
+		if(optCinemaShow.isEmpty()) {
+			// Dieser Fehler sollte im Regelfall nicht auftreten!
+			redirectAttributes.addFlashAttribute("errorMessage",
+				"Die aktuell angezeigte Vorführung konnte in der Datenbank nicht gefunden werden!");
+			return "redirect:/cinema-shows/{id}";
 		}
 
+		CinemaShow cinemaShow = optCinemaShow.get();
+
+		if(optFilmInst.isEmpty()) {
+			// Dieser Fehler sollte im Regelfall nicht auftreten!
+			redirectAttributes.addFlashAttribute("errorMessage", "Der Film existiert nicht!");
+			return "redirect:/cinema-shows/{id}";
+		}
+
+		Film filmInst = optFilmInst.get();
+
+		if(editTime.isBefore(LocalDateTime.now().plusHours(24))) {
+			redirectAttributes.addFlashAttribute("errorMessage",
+				"Die Vorführung muss mind. 24 Stunden in der Zukunft liegen!");
+			return "redirect:/cinema-shows/{id}";
+		}
+
+		if(!filmInst.isAvailableAt(editTime)) {
+			redirectAttributes.addFlashAttribute("errorMessage",
+				"Dieser Film wurde in der Zeit nicht ausgeliehen oder der Ticket-Preis wurde nicht gesetzt!");
+			return "redirect:/cinema-shows/{id}";
+		}
+
+		// Prüfung, ob sich Events oder Vorführungen in dem Vorführungszeitraum und Kinosaal überlappen,
+		// wenn ja Abbruch und Fehler
+		if(!scheduledActivityService.isTimeSlotAvailable(
+			editTime, editTime.plusMinutes(filmInst.getTimePlaying()), cinemaShow.getCinemaHall().getId(), cinemaShow)) {
+			redirectAttributes.addFlashAttribute("errorMessage",
+				"In dem Kinosaal findet von Vorführungs-Beginn, bis Ende " +
+					" bereits eine oder mehrere andere Vorführungen oder Events statt!");
+			redirectAttributes.addFlashAttribute("infoMessage",
+				"Bis 20 min vor und nach Veranstaltungen/Events können keine Vorführungen gebucht werden!");
+			return "redirect:/cinema-shows/{id}";
+		}
+
+		redirectAttributes.addFlashAttribute("successMessage",
+			"Vorführung wurde erfolgreich geändert!");
 		cinemaShowService.update(id)
 			.setStartDateTime(editTime)
 			.setFilm(optFilmInst.get())
@@ -180,9 +252,14 @@ public class ViewProgramController {
 			// TODO Fehlerbehandlung
 			return "redirect:/current-films";
 		}
+		CinemaShow cinemaShow = optionalCinemaShow.get();
+		String redirectUrl = "redirect:/current-films/"
+			+ cinemaShow.getStartDateTime().getYear() + "/"
+			+ AdditionalDateTimeWorker.getWeekOfYear(cinemaShow.getStartDateTime());
+
 		cinemaShowService.deleteCinemaShow(optionalCinemaShow.get());
 
-		return "redirect:/current-films";
+		return redirectUrl;
 	}
 
 	public static class CinemaShowDayEntry {

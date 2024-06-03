a007c7694e3aff5f6dd9045d594a8d3a4c9a849b
git <sl-peace@gmx.de>
2024-06-03 08:16:10 +0200

    Kassensubssystem Fixes #19

--- src/main/java/de/ufo/cinemasystem/controller/MakeOrderController.java
+++ src/main/java/de/ufo/cinemasystem/controller/MakeOrderController.java
@@ -9,8 +9,10 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.regex.Pattern;
 
+import org.salespointframework.order.Cart;
 import org.salespointframework.order.Order;
 import org.salespointframework.order.OrderManagement;
+import org.salespointframework.quantity.Quantity;
 import org.salespointframework.useraccount.UserAccount.UserAccountIdentifier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -18,14 +20,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import de.ufo.cinemasystem.additionalfiles.AdditionalDateTimeWorker;
 import de.ufo.cinemasystem.models.CinemaShow;
 import de.ufo.cinemasystem.models.CinemaShowService;
 import de.ufo.cinemasystem.models.Orders;
 import de.ufo.cinemasystem.models.Seat;
+import de.ufo.cinemasystem.models.Snacks;
 import de.ufo.cinemasystem.models.Ticket;
 import de.ufo.cinemasystem.repository.CinemaShowRepository;
 import de.ufo.cinemasystem.repository.ReservationRepository;
@@ -34,10 +40,12 @@ import de.ufo.cinemasystem.repository.TicketRepository;
 import de.ufo.cinemasystem.repository.UserRepository;
 import jakarta.servlet.http.HttpSession;
 
+
 @Controller
+@SessionAttributes("cart")
 public class MakeOrderController {
 
-	public static final String orderSessionKey = "current-reservation";
+	public static final String orderSessionKey = "current-order";
 
 	private final OrderManagement<Order> orderManagement;
 
@@ -45,6 +53,10 @@ public class MakeOrderController {
 		Assert.notNull(orderManagement, "Order cant be Zero!");
 		this.orderManagement = orderManagement;
 	}
+	@ModelAttribute("cart")
+		Cart initializeCart() {
+			return new Cart();
+		}
 
 //	private @Autowired OrderRepository orderRepo;
 	private @Autowired ReservationRepository reservationRepo;
@@ -54,30 +66,42 @@ public class MakeOrderController {
 	private @Autowired UserRepository userRepo;
 	private @Autowired CinemaShowService showService;
 
-	@GetMapping("/sell-items")
-	public String startOrder(Model m, @AuthenticationPrincipal UserAccountIdentifier currentUser, HttpSession session) {
-		m.addAttribute("title", "Kassensystem");
-		LocalDateTime now = LocalDateTime.now();
-		List<CinemaShow> toOffer = showsRepo
-				.findCinemaShowsInWeek(now.getYear(), AdditionalDateTimeWorker.getWeekOfYear(now)).toList();
-		toOffer = new ArrayList<>(toOffer);
-		Iterator<CinemaShow> iterator = toOffer.iterator();
-
-		while (iterator.hasNext()) {
-			CinemaShow cs = iterator.next();
-			if (LocalDateTime.now().until(cs.getStartDateTime(), ChronoUnit.MILLIS) < Duration.ofMinutes(30)
-					.toMillis()) {
-				iterator.remove();
-			}
+@GetMapping("/sell/ticket")
+public String getMethodName(Model m) {
+	m.addAttribute("title", "Kassensystem");
+	LocalDateTime now = LocalDateTime.now();
+	List<CinemaShow> toOffer = showsRepo.findCinemaShowsInWeek(now.getYear(), AdditionalDateTimeWorker.getWeekOfYear(now)).toList();
+	//unhinge any wannabe-unmodifyables by making a copy to a known-writable list type.
+	toOffer=new ArrayList<>(toOffer);
+	Iterator<CinemaShow> iterator = toOffer.iterator();
+	while(iterator.hasNext()){
+		CinemaShow cs= iterator.next();
+		if(LocalDateTime.now().until(cs.getStartDateTime(), ChronoUnit.MILLIS) < Duration.ofMinutes(30).toMillis()){
+			iterator.remove();
 		}
+	}
+	m.addAttribute("shows", toOffer);
+    return "sell-items-1";
+}
 
-		m.addAttribute("shows", toOffer);
+
+	@GetMapping("/sell-items/{what}")
+	public String startOrder(Model m, @AuthenticationPrincipal UserAccountIdentifier currentUser,
+		@PathVariable CinemaShow what, HttpSession session) {
+		if (session.getAttribute(orderSessionKey) == null) {
+			session.setAttribute(orderSessionKey, new Orders(currentUser, what));
+		}
+		Orders work = (Orders) session.getAttribute(orderSessionKey);
+		m.addAttribute("title", "Kassensystem");
+        m.addAttribute("tickets", work.getOrderLines());
+        m.addAttribute("show", work.getCinemaShow());
+        m.addAttribute("price",work.getTotal());
 		return "sell-items-1";
 	}
 
-	@PostMapping("/sell/ticket")
+	@PostMapping("/sell-items/ticket")
 	public String addTickets(Model m, @AuthenticationPrincipal UserAccountIdentifier currentUser, HttpSession session,
-			CinemaShow show,
+			@RequestParam("show") CinemaShow show,
 			@RequestParam("ticketType") String ticketType, @RequestParam("spot") String spot) {
 
 		if (session.getAttribute(orderSessionKey) == null) {
@@ -95,10 +119,6 @@ public class MakeOrderController {
 				&& !work.getCinemaShow().containsSeat(toRowID(spot), Integer.parseInt(spot.substring(1)))) {
 			errors.add("Ungültiger Sitzplatz: " + spot);
 		}
-		if (LocalDateTime.now().until(work.getCinemaShow().getStartDateTime(), ChronoUnit.MILLIS) < Duration
-				.ofMinutes(30).toMillis()) {
-			errors.add("Reservierungen sind nur bis 30 Minuten vor Vorstellungsbeginn möglich!");
-		}
 		try {
 			if (errors.isEmpty() && showsRepo.findById(work.getCinemaShow().getId()).orElseThrow()
 					.getOccupancy(toRowID(spot), Integer.parseInt(spot.substring(1)))
@@ -109,14 +129,7 @@ public class MakeOrderController {
 			// CinemaShow no longer exists.
 			errors.add("Diese Veranstaltung wurde abgesagt");
 		}
-		/*
-		 * if (toCategoryType(ticketType) == null) {
-		 * errors.add("Nicht existenter Kartentyp");
-		 * }
-		 * // todo: 10 ticket limit for non-staff
-		 * System.out.println("u: " + work.getReservingAccount().getUserAccount());
-		 * System.out.println("t: " + Arrays.toString(work.getTickets()));
-		 */
+		
 		if (errors.isEmpty()) {
 			// add ticket
 			Ticket t = new Ticket(toCategoryType(ticketType), show);
@@ -126,10 +139,21 @@ public class MakeOrderController {
 					new Seat(toRowID(spot), Integer.parseInt(spot.substring(1))), Seat.SeatOccupancy.RESERVED).save();
 		}
 
+
 		return "sell-items-1";
 
 	}
 
+
+	@PostMapping("/sell/snacks")
+	public String addSnacks(Model m, @AuthenticationPrincipal UserAccountIdentifier currentUser, 
+		HttpSession session, @RequestParam("snack") Snacks snack, @ModelAttribute Cart cart) {
+		
+		cart.addOrUpdateItem(snack, Quantity.of(1));
+		
+		return "sell-items-1";
+	}
+	
 	private static int toRowID(String spot) {
 		char rowChar = spot.charAt(0);
 		return rowChar - 'A';

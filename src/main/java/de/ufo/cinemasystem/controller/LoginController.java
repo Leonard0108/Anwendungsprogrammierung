6abd1eade74e7ca737d1decbe272735cbc218279
lukas <lukasd2000@gmx.de>
2024-06-11 14:04:14 +0200

    Cleanerer Code.

--- src/main/java/de/ufo/cinemasystem/controller/LoginController.java
+++ src/main/java/de/ufo/cinemasystem/controller/LoginController.java
@@ -3,18 +3,12 @@ package de.ufo.cinemasystem.controller;
 
 
 import de.ufo.cinemasystem.additionalfiles.RegistrationForm;
-import de.ufo.cinemasystem.additionalfiles.LoginForm;
-import de.ufo.cinemasystem.additionalfiles.RegistrationForm;
 import de.ufo.cinemasystem.additionalfiles.UserService;
 import de.ufo.cinemasystem.models.UserEntry;
-import de.ufo.cinemasystem.repository.EmployeeRepository;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import jakarta.servlet.http.HttpSession;
-import jakarta.validation.Valid;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;

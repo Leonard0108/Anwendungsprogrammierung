be4aa1362777838ecf6c11daf09a64a8f6ade9fc
JannikSchwass <165703008+JannikSchwass@users.noreply.github.com>
2024-06-19 10:29:43 +0200

    bugfixing

--- src/main/java/de/ufo/cinemasystem/controller/IndexController.java
+++ src/main/java/de/ufo/cinemasystem/controller/IndexController.java
@@ -1,16 +1,20 @@
 
 package de.ufo.cinemasystem.controller;
 
-import java.util.ArrayList;
-import java.util.List;
+import de.ufo.cinemasystem.additionalfiles.AdditionalDateTimeWorker;
+import de.ufo.cinemasystem.models.CinemaShow;
+import de.ufo.cinemasystem.models.Film;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import de.ufo.cinemasystem.models.Film;
+import de.ufo.cinemasystem.repository.CinemaShowRepository;
 import de.ufo.cinemasystem.repository.FilmRepository;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Controller for "/"
@@ -21,26 +25,44 @@ import de.ufo.cinemasystem.repository.FilmRepository;
 public class IndexController {
     
     private @Autowired FilmRepository films;
+    private @Autowired CinemaShowRepository csRepo;
     
     /**
-     * TODO: actual index
-     * @return 
+     * index.html
+     * @param m
+     * @return "welcome"
      */
     @GetMapping("/")
     public String getIndexPage(Model m){
-        long count = films.count();
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime end = AdditionalDateTimeWorker.getEndWeekDateTime(now.plusDays(7));
+        List<CinemaShow> options = csRepo.findCinemaShowsInPeriodOfTime(now, end).toList();
+        long count = options.size();
+        ArrayList<Film> defcopy = new ArrayList<>(3);
+        for(CinemaShow c:options){
+            if(!defcopy.contains(c.getFilm())){
+                if(defcopy.size() == 3){
+                    m.addAttribute("hasMore", true);
+                    break;
+                }
+                defcopy.add(c.getFilm());
+            }
+        }
+        /*
         List<Film> toList = films.findAll().toList();
         ArrayList<Film> defcopy = new ArrayList<>(3);
         for(int i = 0; i < toList.size();i++){
-            defcopy.add(toList.get(i));
-            if(i == 2){
-                break;
-            }
+        defcopy.add(toList.get(i));
+        if(i == 2){
+        break;
+        }
         }
         m.addAttribute("filmList", defcopy.toArray());
+        
         if(count >= 4){
             m.addAttribute("hasMore", true);
-        }
+        }*/
+        m.addAttribute("filmList", defcopy.toArray());
         m.addAttribute("title", "Startseite");
         return "welcome";
     }

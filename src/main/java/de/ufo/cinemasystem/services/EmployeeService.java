083aa66027bd6711a1e7fffc66148d9052057bac
Lukas <lukasd2000@gmx.de>
2024-06-22 11:12:59 +0200

    Erste große Änderung im Code - Überprüfung der Stunden und es Gehalts.

--- src/main/java/de/ufo/cinemasystem/services/EmployeeService.java
+++ src/main/java/de/ufo/cinemasystem/services/EmployeeService.java
@@ -59,58 +59,69 @@ public class EmployeeService {
 		return false;
 	}
 
-		public short createEmployee(EmployeeRegistrationForm employeeRegistrationForm)
-		{
-			Assert.notNull(employeeRegistrationForm, "Registration form must not be null!");
+	public short createEmployee(EmployeeRegistrationForm employeeRegistrationForm)
+	{
+		Assert.notNull(employeeRegistrationForm, "Registration form must not be null!");
 
-			if (!isKnownEmailProvider(employeeRegistrationForm.getEMail())) {
-				return 4;  // Return a new code for unknown email providers
-			}
-
-
-			if (employeeRepository.findByJobMail(employeeRegistrationForm.getJobMail()).isPresent())
-			{
-				return 1;
-			}
-
-			short hoursPerWeek = Short.parseShort(employeeRegistrationForm.getHoursPerWeek());
-			String salaryCleaned = employeeRegistrationForm.getSalary().replaceAll("[€,]", "");
-			Long salaryLong = Long.valueOf(salaryCleaned);
-			BigDecimal salaryAmount = BigDecimal.valueOf(salaryLong);
-			CurrencyUnit currency = Monetary.getCurrency("EUR");
-			Money salary = Money.of(salaryAmount, currency);
-
-			if (!employeeRegistrationForm.getJobMail().endsWith("@ufo-kino.de") || employeeRepository.findByJobMail(employeeRegistrationForm.getJobMail()).isPresent()) {
-				return 2;
-			}
-
-			if (hoursPerWeek > 50 || salary.isNegative() || salary.isZero() || salary.divide(hoursPerWeek * 4).isLessThan(Money.of(12, salary.getCurrency()))) {
-				return 3;
-			}
-
-
-
-			UserEntry userEntry = userRepository.findByeMail(employeeRegistrationForm.getEMail());
-
-			if (userEntry == null /*&& userRepository.findByUserAccountEmail(employeeRegistrationForm.getEMail()) == null*/) {
-				UserAccount userAccount = userAccountManagement.create(employeeRegistrationForm.getUsername(), Password.UnencryptedPassword.of(employeeRegistrationForm.getPassword()), Role.of("EMPLOYEE"));
-				userAccount.setFirstname(employeeRegistrationForm.getFirstName());
-				userAccount.setLastname(employeeRegistrationForm.getLastName());
-				userAccount.setEmail(employeeRegistrationForm.getEMail());
-
-				userEntry = new UserEntry(userAccount, employeeRegistrationForm.getFirstName(), employeeRegistrationForm.getLastName(), employeeRegistrationForm.getEMail(), employeeRegistrationForm.getStreetName(), employeeRegistrationForm.getHouseNumber(), employeeRegistrationForm.getCity(), employeeRegistrationForm.getPostalCode(), employeeRegistrationForm.getState(), employeeRegistrationForm.getCountry());
-
-				userRepository.save(userEntry);
-			}
-
-			EmployeeEntry employeeEntry = new EmployeeEntry(userEntry, salary, employeeRegistrationForm.getJobMail(), hoursPerWeek);
-			employeeRepository.save(employeeEntry);
-
-			return 0;
+		if (!isKnownEmailProvider(employeeRegistrationForm.getEMail())) {
+			return 4;  // Return a new code for unknown email providers
 		}
 
 
-	public void editEmployee(UserEntry.UserIdentifier id, String firstName, String lastName, String email, String job, String salary, String hours)
+		if (employeeRepository.findByJobMail(employeeRegistrationForm.getJobMail()).isPresent())
+		{
+			return 1;
+		}
+
+		short hoursPerWeek = Short.parseShort(employeeRegistrationForm.getHoursPerWeek());
+		String salaryCleaned = employeeRegistrationForm.getSalary().replaceAll("[€,]", "");
+		Long salaryLong = Long.valueOf(salaryCleaned);
+		BigDecimal salaryAmount = BigDecimal.valueOf(salaryLong);
+		CurrencyUnit currency = Monetary.getCurrency("EUR");
+		Money salary = Money.of(salaryAmount, currency);
+
+		if (!employeeRegistrationForm.getJobMail().endsWith("@ufo-kino.de") || employeeRepository.findByJobMail(employeeRegistrationForm.getJobMail()).isPresent()) {
+			return 2;
+		}
+
+		if (hoursPerWeek > 50 || salary.isNegative() || salary.isZero() || salary.divide(hoursPerWeek * 4).isLessThan(Money.of(12, salary.getCurrency()))) {
+			return 3;
+		}
+
+
+
+		UserEntry userEntry = userRepository.findByeMail(employeeRegistrationForm.getEMail());
+
+		if (userEntry == null /*&& userRepository.findByUserAccountEmail(employeeRegistrationForm.getEMail()) == null*/) {
+			UserAccount userAccount = userAccountManagement.create(employeeRegistrationForm.getUsername(), Password.UnencryptedPassword.of(employeeRegistrationForm.getPassword()), Role.of("EMPLOYEE"));
+			userAccount.setFirstname(employeeRegistrationForm.getFirstName());
+			userAccount.setLastname(employeeRegistrationForm.getLastName());
+			userAccount.setEmail(employeeRegistrationForm.getEMail());
+
+			userEntry = new UserEntry(userAccount, employeeRegistrationForm.getFirstName(), employeeRegistrationForm.getLastName(), employeeRegistrationForm.getEMail(), employeeRegistrationForm.getStreetName(), employeeRegistrationForm.getHouseNumber(), employeeRegistrationForm.getCity(), employeeRegistrationForm.getPostalCode(), employeeRegistrationForm.getState(), employeeRegistrationForm.getCountry());
+
+			userRepository.save(userEntry);
+		}
+
+		EmployeeEntry employeeEntry = new EmployeeEntry(userEntry, salary, employeeRegistrationForm.getJobMail(), hoursPerWeek);
+		employeeRepository.save(employeeEntry);
+
+		return 0;
+	}
+
+
+	/**
+	 *
+	 * @param id id of the to edit employee account
+	 * @param firstName first name of the  employee. Changeable because of gender diversity.
+	 * @param lastName last name of the  employee. Changeable because of marriage.
+	 * @param email maybe the employee changes him/Z her main e-mail-address
+	 * @param job changes the role of the employee
+	 * @param salary in- or decrease the salary
+	 * @param hours changes the number of hours
+	 * @return 0 if everything works out, 1 if there are negative hours, 2 if to many hours and two if the salary is too bad.
+	 */
+	public int editEmployee(UserEntry.UserIdentifier id, String firstName, String lastName, String email, String job, String salary, String hours)
 	{
 		UserEntry     userEntry     = userRepository.findById(id).orElseThrow();
 		EmployeeEntry employeeEntry = employeeRepository.findById(id).orElseThrow();
@@ -134,18 +145,42 @@ public class EmployeeService {
 			userEntry.getUserAccount().add(Role.of("AUTHORIZED_EMPLOYEE"));
 			userEntry.getUserAccount().remove(Role.of("EMPLOYEE"));
 		}
+
+
+		if (hours != null && !hours.isEmpty()) {
+			short hoursPerWeek = Short.parseShort(hours);
+			if (hoursPerWeek < 0) {
+				return 1;
+			}
+
+			if (hoursPerWeek > 50)
+			{
+				return 2;
+			}
+
+			employeeEntry.setHoursPerWeek(hoursPerWeek);
+		}
+
+
 		if (salary != null && !salary.isEmpty()) {
 			String salaryCleaned = salary.replaceAll("[€,]", "");
-			Long salaryLong = Long.valueOf(salaryCleaned);
+			long salaryLong = Long.parseLong(salaryCleaned);
+
+			if (salaryLong < 1 || (salaryLong / employeeEntry.getHoursPerWeek() < 12))
+			{
+				return 3 ;
+			}
+
+
 			BigDecimal salaryAmount = BigDecimal.valueOf(salaryLong);
 			CurrencyUnit currency = Monetary.getCurrency("EUR");
 			Money finishedSalary = Money.of(salaryAmount, currency);
 
 			employeeEntry.setSalary(finishedSalary);
 		}
-		if (hours != null && !hours.isEmpty()) {
-			employeeEntry.setHoursPerWeek(Short.parseShort(hours));
-		}
+
 		employeeRepository.save(employeeEntry);
+
+		return 0;
 	}
 }
